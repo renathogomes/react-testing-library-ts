324894416f2d8097bfc936a9eeb4456c512b0a08
renatho <renatho@pop-os.localdomain>
2023-06-27 17:45:11 -0300

    testando

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,1 +1,14 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação:', () => {
+  renderWithRouter(<App />);
+  const HomeLink = screen.getByRole('link', { name: /home/i });
+  const AboutLink = screen.getByRole('link', { name: /about/i });
+  const FavoriteLink = screen.getByRole('link', { name: /favorite pokemon/i });
+
+  expect(HomeLink).toBeInTheDocument();
+  expect(AboutLink).toBeInTheDocument();
+  expect(FavoriteLink).toBeInTheDocument();
+});
