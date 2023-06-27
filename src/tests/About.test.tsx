4dc0459eca2597a14d5239d325cc59f06996dbcc
renatho <renatho@pop-os.localdomain>
2023-06-27 19:12:10 -0300

    req 2 acho que esta certo

--- src/tests/About.test.tsx
+++ src/tests/About.test.tsx
@@ -1,1 +1,16 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { About } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o requisito 2', () => {
+  test('Testar se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const AboutPage = screen.getByText('About Pokédex', { selector: 'h2' });
+    const Img = screen.getByRole('img');
+    const srcImp = Img.getAttribute('src');
+
+    expect(AboutPage).toBeInTheDocument();
+    expect(srcImp).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
