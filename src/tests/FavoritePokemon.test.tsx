5be2a868e9e19cde314ca651d28e92088a610a34
renatho <renatho@pop-os.localdomain>
2023-06-28 10:30:37 -0300

    req 3

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,13 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { FavoritePokemon } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o requisito 3', () => {
+  test('Testa se a mensagem "No favorite pokemon found" é exibida na tela caso a pessoa não tenha Pokémon favorito ', () => {
+    renderWithRouter(<FavoritePokemon />);
+
+    const FavoritText = screen.getByText(/No favorite pokémon found/i);
+
+    expect(FavoritText).toBeInTheDocument();
+  });
+});
