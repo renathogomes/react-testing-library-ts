e84dda20331685e787bb766cc75512751c50e185
renatho <renatho@pop-os.localdomain>
2023-07-05 13:59:33 -0300

    um commit

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -4,10 +4,18 @@ import renderWithRouter from '../renderWithRouter';
 
 describe('Testa o requisito 3', () => {
   test('Testa se a mensagem "No favorite pokemon found" é exibida na tela caso a pessoa não tenha Pokémon favorito ', () => {
+    renderWithRouter(<FavoritePokemon pokemonList={ [] } />);
+
+    const FavoriteFound = screen.getByText(/No favorite pokémon found/i);
+
+    expect(FavoriteFound).toBeInTheDocument();
+  });
+
+  test('Testa se apenas são exibidos os Pokémon favoritados', () => {
     renderWithRouter(<FavoritePokemon />);
 
-    const FavoritText = screen.getByText(/No favorite pokémon found/i);
+    const MoreDetail = screen.getByRole('link', { name: /more details/i });
 
-    expect(FavoritText).toBeInTheDocument();
+    expect(MoreDetail).toBeInTheDocument();
   });
 });
