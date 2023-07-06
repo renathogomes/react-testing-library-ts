a5e5ffb7473195cf8528abc7bb30b147f489dd0c
renatho <renatho@pop-os.localdomain>
2023-07-06 11:27:19 -0300

    pokedex em andamento

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -1,1 +1,27 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import pokemonList from '../data';
+import { Pokedex } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando Pokedex', () => {
+  test('Testa se existe um h2 com o título "Encountered Pokémon"', () => {
+    renderWithRouter(<Pokedex
+      pokemonList={ pokemonList }
+      favoritePokemonIdsObj={ {} }
+    />);
+
+    const heading = screen.getByRole('heading', { level: 2 });
+    expect(heading).toHaveTextContent(/Encountered Pokémon/i);
+  });
+
+  test('Testa se exibe o próximo pokemon depois de clicar no botao', () => {
+    renderWithRouter(<Pokedex
+      pokemonList={ pokemonList }
+      favoritePokemonIdsObj={ {} }
+    />);
+
+    const nextPokemonId = screen.getByTestId('next-pokemon');
+    expect(nextPokemonId).toHaveTextContent(/Próximo Pokémon/i);
+    expect(nextPokemonId).toBeInTheDocument();
+  });
+});
